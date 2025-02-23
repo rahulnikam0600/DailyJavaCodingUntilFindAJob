510aab39b7196dbda02878681413cde11b131c3f
Rahul Nikam <errah@Mac.lan>
2025-02-23 15:15:12 +0530

    Day 11 code added Find the cube of each element from list and print the cubes greater than 50

--- DailyCodingUntilGetJob/src/com/example/day11/App.java
+++ DailyCodingUntilGetJob/src/com/example/day11/App.java
@@ -12,5 +12,16 @@ import java.util.List;
 import java.util.Arrays;
 
 public class App{
+	
+	public static void main(String args[]){
 		
+		List<Integer> numbers = Arrays.asList(3,6,4,5,1,2,7,8,9);
+		
+		numbers.stream()
+			.map(e -> e*e*e)
+			.filter(e -> e > 50)
+			.forEach(System.out::println);
+			
+	}
+	
 }
