3e2bb86b49fc417ba1c453b9f8bb5258905dbbec
Rahul Nikam <errah@Mac.lan>
2025-02-23 15:10:45 +0530

    Day 10 code is added Print the cube of elements of array which are greater than 50

--- DailyCodingUntilGetJob/src/com/example/day10/App.java
+++ DailyCodingUntilGetJob/src/com/example/day10/App.java
@@ -1,9 +1,30 @@
 package com.example.day10;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+/*
+ * Day 10
+ * Daily Java Coding Until I Find A Job
+ * Find the Duplicates from below list of string?
+ * Input: List<String> words = Arrays.asList("AA", "BB", "CC","AA");
+ * Output: AA
+ *
+*/
 
-public class App {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+
+public class App{
+	
+	public static void main(String args[]){
+		
+		List<String> words = Arrays.asList("AA", "BB", "CC","AA");
+		
+		words.stream()
+			.filter(e -> Collections.frequency(words, e) > 1)
+			.distinct()
+			.forEach(System.out::println);
+		
+	}
 	
 }
+
