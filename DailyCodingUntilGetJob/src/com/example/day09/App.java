910f38dd2e3be29a6761fb2663a0cf2d2b4e4798
rahulnikam0600 <rahulnikam0600@gmail.com>
2025-02-25 11:50:17 +0530

    Day9 code added

---
+++ DailyCodingUntilGetJob/src/com/example/day09/App.java
@@ -0,0 +1,29 @@
+package com.example.day09;
+
+/*
+ * Day 9
+ * Daily Java Coding Until I Find A Job
+ * Print the duplicate values from array
+ * Input: (1,4,6,7,8,9,4,5,4,2,2)
+ * Output: 4 2
+ *
+*/
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class App{
+	
+	public static void main(String args[]){
+		
+		List<Integer> numbers = Arrays.asList(1,4,6,7,8,9,4,5,4,2,1,2);
+		
+		numbers.stream()
+			.filter(x -> Collections.frequency(numbers, x) > 1)
+			.distinct()
+			.forEach(r -> System.out.print(r+" "));
+		
+	}
+	
+}
